01ddfa464635a33b90375360f1234f2f8714fe70
unknown <20PW14@cs2k16.amcs.ws2k12.psgcs.edu>
2022-08-03 10:08:00 +0530

    20XW58 : changes to GDAlgo

--- 20XW58 - MACHINE LEARNING LAB/GradientDescent/SoapNSud/main.py
+++ 20XW58 - MACHINE LEARNING LAB/GradientDescent/SoapNSud/main.py
@@ -1,55 +1,37 @@
 import matplotlib.pyplot as plt
 import numpy as np
+xy = [(4,33), (4.5, 42), (5, 45), (5.5,51), (6, 53), (6.5, 61), (7, 62)]
 
-# X = np.array([4, 4.5, 5, 5.5, 6, 6.5, 7])
-# y = np.array([33, 42, 45, 51, 53, 61, 62])
-
-
-xySet = np.array([(4, 33), (4.5, 42), (5, 45),
-                 (5.5, 51), (6, 53), (6.5, 61), (7, 62)])
+ogX = np.array([4, 4.5, 5, 5.5, 6, 6.5, 7])
 
 
-η = 0.1
+eta = 0.0088315
 Bold = [0, 0]
 Bnew = [0, 0]
-print(f"b0 = {Bold[0]}, b1 = {Bold[1]}")
 
-iterations = 0
+iterations=0
 while True:
-    gradSSE = [0, 0]
-    for (xi, yi) in xySet:
-        gradSSE[0] += Bold[0] - Bold[1]*xi
-        gradSSE[1] += (Bold[0] - Bold[1]*xi)*xi
-    Bnew[0] = Bold[0] - η*gradSSE[0]
-    Bnew[1] = Bold[1] - η*gradSSE[1]
-    iterations += 1
-    print(f"b0 = {Bnew[0]}, b1 = {Bnew[1]}")
-    # if abs(Bnew[0] - Bold[0]) < 0.0001 and abs(Bnew[1] - Bold[1]) < 0.0001:
-    #     break
-
+    grad = [0, 0]
+    for xi,yi in xy:
+        grad[0] += (yi - Bold[0] -Bold[1]*xi)
+        grad[1] += (yi - Bold[0] -Bold[1]*xi)*xi
+        
+    
+    Bnew[0] = Bold[0] + eta*grad[0]
+    Bnew[1] = Bold[1] + eta*grad[1]
+    
+    
+    iterations+=1
+    if Bold==Bnew:
+        break
+    
     Bold[0] = Bnew[0]
     Bold[1] = Bnew[1]
-print(f"b0 = {Bnew[0]}, b1 = {Bnew[1]} in {iterations} iterations.")
 
 # Plot the estimator
-# estimatorFn = B[1]*ogX + B[0]
-# plt.plot(ogX, estimatorFn)
-# plt.show()
-
-
-# yDash = estimatorFn.mean()
-
-# #SSE, SST, and SSR.
-# SSE = 0
-# SST = 0
-# SSR = 0
-# SumOfErr = 0
-
-# for i in range(len(ogX)):
-#     SSE += (y[i] - (B[1]*ogX[i] + B[0])) ** 2
-#     SST += (y[i] - yDash) ** 2
-#     SSR += ((B[1]*ogX[i] + B[0]) - yDash) ** 2
-#     SumOfErr += y[i] - (B[1]*ogX[i] + B[0])
-# print(f"\nSum of errors = {SumOfErr}, \nSSE = {SSE}, \nSST = {SST}, \nSSR = {SSR}")
-
-# print(f"SSE + SSR = {SSE+SSR} == {SST} = SST")
+estimatorFn = Bold[1]*ogX + Bold[0]
+plt.plot(ogX,estimatorFn)
+# print(iterations)
+plt.ylim((30, 70))    
+plt.show()
+print(f"Bold in {iterations}th iteration : {Bold}")
