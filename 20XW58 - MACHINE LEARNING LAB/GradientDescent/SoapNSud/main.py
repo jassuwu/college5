7f8ef83852620d93d90eb517777b6c298beb8c18
20PW14 <karthickprnv@gmail.com>
2022-08-03 07:53:19 +0530

    20XW58 : changes to GDAlgo

--- 20XW58 - MACHINE LEARNING LAB/GradientDescent/SoapNSud/main.py
+++ 20XW58 - MACHINE LEARNING LAB/GradientDescent/SoapNSud/main.py
@@ -5,30 +5,36 @@ import numpy as np
 # y = np.array([33, 42, 45, 51, 53, 61, 62])
 
 
-xySet = np.array([(4,33), (4.5,42), (5,45), (5.5, 51), (6,53), (6.5, 61), (7, 62)])
-
+xySet = np.array([(4, 33), (4.5, 42), (5, 45),
+                 (5.5, 51), (6, 53), (6.5, 61), (7, 62)])
 
 
 η = 0.1
-Bold = [0,0]
-Bnew = [0,0]
-print(f"b0 = {B[0]}, b1 = {B[1]}")
+Bold = [0, 0]
+Bnew = [0, 0]
+print(f"b0 = {Bold[0]}, b1 = {Bold[1]}")
 
-
+iterations = 0
 while True:
-    gradSSE = [0,0]
-    for (xi,yi) in xySet:
-        gradSSE[0] += B[0] - B[1]*xi
-        gradSSE[1] += (B[0] - B[1]*xi)*xi
-
+    gradSSE = [0, 0]
+    for (xi, yi) in xySet:
+        gradSSE[0] += Bold[0] - Bold[1]*xi
+        gradSSE[1] += (Bold[0] - Bold[1]*xi)*xi
     Bnew[0] = Bold[0] - η*gradSSE[0]
-    Bnew[1] = Bold[1] - η*gradSSE[1]    
+    Bnew[1] = Bold[1] - η*gradSSE[1]
+    iterations += 1
+    print(f"b0 = {Bnew[0]}, b1 = {Bnew[1]}")
+    # if abs(Bnew[0] - Bold[0]) < 0.0001 and abs(Bnew[1] - Bold[1]) < 0.0001:
+    #     break
 
+    Bold[0] = Bnew[0]
+    Bold[1] = Bnew[1]
+print(f"b0 = {Bnew[0]}, b1 = {Bnew[1]} in {iterations} iterations.")
 
 # Plot the estimator
 # estimatorFn = B[1]*ogX + B[0]
 # plt.plot(ogX, estimatorFn)
-# plt.show()      
+# plt.show()
 
 
 # yDash = estimatorFn.mean()
@@ -36,7 +42,7 @@ while True:
 # #SSE, SST, and SSR.
 # SSE = 0
 # SST = 0
-# SSR = 0 
+# SSR = 0
 # SumOfErr = 0
 
 # for i in range(len(ogX)):
