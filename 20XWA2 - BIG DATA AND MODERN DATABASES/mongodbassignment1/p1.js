5b872d78a6fb2afb10f388e2b90d69b868ad20b1
20PW14 <karthickprnv@gmail.com>
2022-09-07 07:53:19 +0530

    20XWA2 : start mongodbassignment1

---
+++ 20XWA2 - BIG DATA AND MODERN DATABASES/mongodbassignment1/p1.js
@@ -0,0 +1,93 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Connect to MongoDB Atlas using Mongoose
+const main = async () => {
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log('Connected to MongoDB Atlas.')
+}
+main().catch(err => console.log(err));
+
+
+// Modelling the diversion schema
+const diversionSchema = new mongoose.Schema({
+    name: String,
+    diversions: Number,
+    year: Number,
+    month: Number
+});
+const Diversion = mongoose.model("Diversion", diversionSchema);
+
+// Functionalities
+const populate = async () => {
+    await Diversion.insertMany([{
+        name: 'A',
+        diversions: 31,
+        year: 2001,
+        month: 1
+    }, {
+        name: 'B',
+        diversions: 62,
+        year: 2002,
+        month: 2
+    }, {
+        name: 'C',
+        diversions: 20,
+        year: 2003,
+        month: 3
+    }, {
+        name: 'D',
+        diversions: 25,
+        year: 2004,
+        month: 4
+    }, {
+        name: 'E',
+        diversions: 92,
+        year: 2005,
+        month: 5
+    }
+    ]);
+    console.log(docs);
+    console.log('Data inserted.');
+}
+const findAllRecords = async () => {
+    records = await Diversion.find();
+    console.log(records);
+}
+const readAndAggregate = async () => {
+    const records = await Diversion.aggregate([
+        {
+            $group: {
+                _id: "$name",
+                total: { $sum: "$diversions" }
+            }
+        }
+    ]);
+    console.log(records);
+}
+const updateRecords = async () => {
+    const records = await Diversion.updateMany({ name: /[AB]/ }, { $set: { diversions: 69 } });
+    console.log(records);
+}
+const deleteRecords = async () => {
+    const records = await Diversion.deleteMany({ name: /[AB]/ });
+    console.log(records);
+}
+
+
+
+// Populating the database with user-data
+populate().catch(err => console.log(err));
+
+// Querying the database
+findAllRecords().catch(err => console.log(err));
+
+// Read + Aggregation
+readAndAggregate().catch(err => console.log(err));
+
+// Update records
+updateRecords().catch(err => console.log(err));
+
+// Delete records
+deleteRecords().catch(err => console.log(err));
